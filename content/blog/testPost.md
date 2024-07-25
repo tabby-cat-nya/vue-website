eb616ab42f1d6203f5deb71dc8e78efa3bce0c46
Clevetop <tom@clevertop.dev>
2024-07-25 21:37:22 +1000

    readded provacy policy

--- content/blog/testPost.md
+++ content/blog/testPost.md
@@ -3,3 +3,5 @@
 Markdown test post 
 
 - meow
+
+dsadsa
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,16 @@
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const post = await $content('blog', params.slug).fetch()
+
+      return { post }
+    }
+  }
+</script>
+
+<template>
+    <main>
+      <ContentDoc />
+    </main>
+  </template>
+  
