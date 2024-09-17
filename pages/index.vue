a3dcc94d5951fa3ec114306a7a5cbaa08c662073
Clevertop <tom@clevertop.dev>
2024-09-17 13:12:33 +1000

    updated currently working on

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ var { data } = await useAsyncData('home', () => queryContent('blog').sort({id:-1
             <h1>Heyo! I'm <Purple>Tom</Purple> 👋</h1>
             <p>I'm a Games Developer from Sydney, Australia.</p>
             <p>I've been programming since 2015 and am currently in my final year of the Games Development degree at UTS</p>
-            <p>Currently im working with my group, <a href="https://discord.gg/3BPYMHqNve">Team Stingray</a>, to create <a href="https://teamstingray.dev/arcane-raiders">Arcane Raiders</a>! Feel free to join our discord server if you'd like to follow along with development!</p>
+            <p>Currently im working with my group, <a href="https://discord.gg/3BPYMHqNve">Team Stingray</a>, to create a new racing game about modding your car during the race and continuining to work on <a href="https://teamstingray.dev/arcane-raiders">Arcane Raiders</a> ready for showcase at SXSW Sydney! Feel free to join our discord server if you'd like to follow along with development!</p>
             <Socials>
                 <Social 
                     src="img/github.svg" 
