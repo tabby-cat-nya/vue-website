0461e78edb0b1dc3bf2ea445361940b35c661a73
Clevertop <tom@clevertop.dev>
2024-09-21 21:24:43 +1000

    add speed analytics

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
+
 <template>
+  <SpeedInsights />
   <div>
     <!-- <NuxtRouteAnnouncer />
     <NuxtWelcome /> -->
