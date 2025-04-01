9a94c08e6c135f159d7740e4674cd9821d6a7a30
Tabby <41929769+tabby-cat-nya@users.noreply.github.com>
2025-04-01 23:02:24 +1100

    added slight padding and a link to tabbycat dev home

--- pages/homepage.vue
+++ pages/homepage.vue
@@ -1,14 +1,16 @@
 <template>
     <main>
         <div class="container">
+            <i><a href="http://tabbycat.dev/"> hop to tabbycat.dev</a></i>
+
             <h1>Tabbybox</h1>
-            <a href="http://tabbybox:8096/">Jellyfin</a> - 
+            <a href="http://tabbybox:8096/">Jellyfin</a> • 
             <a href="http://tabbybox:1880/">Node Red</a>
 
             <h1>Instagram Feeds</h1>
-            <a href="https://www.instagram.com/crap.uts/">CRAP</a> - 
-            <a href="https://www.instagram.com/usydcatsoc/">Catsoc</a> - 
-            <a href="https://www.instagram.com/usyd_dbsoc/">Baking Soc</a> - 
+            <a href="https://www.instagram.com/crap.uts/">CRAP</a> • 
+            <a href="https://www.instagram.com/usydcatsoc/">Catsoc</a> • 
+            <a href="https://www.instagram.com/usyd_dbsoc/">Baking Soc</a> • 
             <a href="https://www.instagram.com/twenty10nsw/">Twenty10</a> 
         </div>
     </main>
@@ -17,5 +19,7 @@
 </template>
 
 <style>
-
+    .container{
+        padding-left: 1cm;
+    }
 </style>
