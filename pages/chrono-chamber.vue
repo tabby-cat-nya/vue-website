3f808b7b2c2295f4c12e8d2cf4390495b22fc102
Tabby <41929769+tabby-cat-nya@users.noreply.github.com>
2025-04-06 11:49:51 +1000

    chrono chamber base page

--- pages/chrono-chamber.vue
+++ pages/chrono-chamber.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1>Chrono Chamber</h1>
+    Hey there, if youre part of the smash staff i will be updating this page shortly to have links to the game, info and a way to play it!
+    Once its ready i imagine it will look something like this: <a href="https://teamstingray.dev/arcane">teamstingray.dev/arcane</a>
     Chrono chamber landing page/redirect to steam, probably like teamstingray.dev/arcane
   </div>
 </template>
