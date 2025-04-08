0c39c7da0640b89afaf56789bc4be74b3321b8c8
Tabby <41929769+tabby-cat-nya@users.noreply.github.com>
2025-04-08 11:46:01 +1000

    update homepage

--- pages/homepage.vue
+++ pages/homepage.vue
@@ -2,12 +2,21 @@
     <main>
         <div class="container">
             <i><a href="http://tabbycat.dev/"> hop to tabbycat.dev</a></i>
+            <p>Dont forget to take your estrogen and cypro! 🏳️‍⚧️</p>
 
             <h1>Tabbybox</h1>
             <a href="http://tabbybox:8096/">Jellyfin</a> • 
             <a href="http://tabbybox:1880/">Node Red</a>
 
-            <h1>Instagram Feeds</h1>
+            <h1>Links</h1>
+            <a href="https://mail.google.com/mail/u/0/#inbox">E-mail</a> • 
+            <a href="https://calendar.google.com">Calendar</a> 
+            
+            <h2>Chats</h2>
+            <a href="https://web.whatsapp.com/">Whatsapp</a> • 
+            <a href="https://web.telegram.org/a/">Telegram</a>
+            
+            <h2>Instagram Feeds</h2>
             <a href="https://www.instagram.com/crap.uts/">CRAP</a> • 
             <a href="https://www.instagram.com/usydcatsoc/">Catsoc</a> • 
             <a href="https://www.instagram.com/usyd_dbsoc/">Baking Soc</a> • 
