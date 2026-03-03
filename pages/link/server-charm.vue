37d7a0f83cc2cfdc9cb54e8cce03ab2e5d407ebc
Tabby <41929769+tabby-cat-nya@users.noreply.github.com>
2026-03-03 03:32:25 +0000

    added server charm slides redirect

---
+++ pages/link/server-charm.vue
@@ -0,0 +1,9 @@
+<script>
+    navigateTo("https://docs.google.com/presentation/d/1Y7x4CKQbvh-I2nDbcyf-wUChW0C2cpDNMP5VHZoY5Ac/edit?usp=sharing", { external: true });
+    
+</script>
+
+<template>
+    <p>Redirecting...</p>
+
+</template>
